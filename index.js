78a81cffc00c22c4c5a4ef2253e56ed101c35577
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2020-12-07 20:44:45 -0500

    set up server

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code away!
+
+const express = require("express")
+const morgan = require("morgan")
+const userRouter = require("./users/userRouter")
+const postRouter = require("./posts/postRouter")
+
+const server = express()
+const port = 4000
+
+server.use(express.json())
+
+server.use((req, res, next) => {
+    const time = now Date().toISOString()
+    console.log(`[${time}] ${req.ip} ${req.method} ${req.url}`)
+    next()
+})
+
+server.use(userRouter)
+server.use(postRouter)
+
+server.listen(port, ()=> {
+    console.log(`Server running at http://localhost:${port}`)
+})
